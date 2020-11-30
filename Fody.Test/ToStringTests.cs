7def033cb442a14e0fa90bfbf5611bb456ff7eb9
Harald Wilbertz <harald.wilbertz@gmx.de>
2020-11-30 10:01:58 +0100

    First unit tests.

---
+++ Fody.Test/ToStringTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace Fody.Test
+{
+    public class ToStringTests
+    {
+        [Fact]
+        public void ToString_Should_Work()
+        {
+            // Arrange 
+            var unitUnderTest = new ManualToString {Name = "Harald", HomeTown = "Berlin", Age = 59, Password = "SuperSecret"};
+
+            // Act
+            var result = unitUnderTest.ToString();
+
+            // Assert
+
+        }
+    }
+}
---
+++ FodyClass/ManualToString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fody
+{
+    public class ManualToString
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+        public string Password { get; set; }
+    }
+}
