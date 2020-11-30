770f17f093a22ff88c8aa15a87a71911eb63ab5d
Harald Wilbertz <harald.wilbertz@gmx.de>
2020-11-30 09:50:30 +0100

    Add project files.

---
+++ Fody/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fody
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
